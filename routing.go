17a57a883cd5cca8a9fe087540f6d9f8c39faf3c
DavidCai <davidcai1993@yahoo.com>
2016-12-03 11:21:44 +0800

    optimize Match

--- routing.go
+++ routing.go
@@ -8,10 +8,24 @@ import (
 )
 
 // Version is this package's version number.
-const Version = "0.0.1"
+const Version = "1.0.0"
+
+var (
+	normalStrReg    = regexp.MustCompile(`^[\w\.-]+$`)
+	separatedStrReg = regexp.MustCompile(`^[\w\.\-][\w\.\-\|]+[\w\.\-]$`)
+	nameStrReg      = regexp.MustCompile(`^\:\w+\b`)
+	surroundStrReg  = regexp.MustCompile(`^\(.+\)$`)
+)
+
+type option struct {
+	name string
+	str  string
+	reg  string
+}
 
 // Node represents a node in a trie.
 type Node struct {
+	Callback interface{}
 	parent   *Node
 	children map[option]*Node
 }
@@ -25,45 +39,11 @@ func New() *Node {
 }
 
 // Define defines a url
-func (n *Node) Define(u string) {
-	n.define(strings.Split(checkURL(u), "/")[1:])
+func (n *Node) Define(u string, callback interface{}) {
+	n.define(strings.Split(checkURL(u), "/")[1:], callback)
 }
 
-// Match matchs a url
-func (n *Node) Match(u string) (map[string]string, bool) {
-	u, err := url.QueryUnescape(u)
-
-	if err != nil {
-		return nil, false
-	}
-
-	return n.match(map[string]string{}, strings.Split(checkURL(u), "/")[1:])
-}
-
-func (n *Node) match(p map[string]string, frags []string) (map[string]string, bool) {
-	frag := frags[0]
-
-	for opt, child := range n.children {
-		if opt.str == frag ||
-			(opt.reg != "" && regexp.MustCompile(opt.reg).MatchString(frag)) {
-			if opt.name != "" {
-				p[opt.name] = frag
-			}
-
-			if len(frags) == 1 {
-				return p, true
-			}
-
-			if p, ok := child.match(p, frags[1:]); ok {
-				return p, ok
-			}
-		}
-	}
-
-	return nil, false
-}
-
-func (n *Node) define(frags []string) {
+func (n *Node) define(frags []string, callback interface{}) {
 	options := parse(frags[0])
 
 	nodes := []*Node{}
@@ -79,12 +59,51 @@ func (n *Node) define(frags []string) {
 	}
 
 	if len(frags) == 1 {
+		for _, node := range nodes {
+			node.Callback = callback
+		}
+
 		return
 	}
 
 	for _, node := range nodes {
-		node.define(frags[1:])
+		node.define(frags[1:], callback)
 	}
+}
+
+// Match matchs a url
+func (n *Node) Match(u string) (*Node, map[string]string, bool) {
+	u, err := url.QueryUnescape(u)
+
+	if err != nil {
+		return nil, nil, false
+	}
+
+	return n.match(map[string]string{}, strings.Split(checkURL(u), "/")[1:])
+}
+
+func (n *Node) match(p map[string]string, frags []string) (*Node, map[string]string, bool) {
+	frag := frags[0]
+
+	for opt, child := range n.children {
+		if opt.str == frag ||
+			(opt.reg != "" && regexp.MustCompile(opt.reg).MatchString(frag)) ||
+			opt.name != "" {
+			if opt.name != "" {
+				p[opt.name] = frag
+			}
+
+			if len(frags) == 1 {
+				return child, p, true
+			}
+
+			if c, p, ok := child.match(p, frags[1:]); ok {
+				return c, p, ok
+			}
+		}
+	}
+
+	return nil, nil, false
 }
 
 func (n *Node) find(opt option) *Node {
@@ -104,6 +123,54 @@ func (n *Node) attach(opt option) *Node {
 	n.children[opt] = node
 
 	return node
+}
+
+func parse(frag string) []option {
+	if frag == "" || normalStrReg.MatchString(frag) {
+		return []option{option{str: frag}}
+	}
+
+	if separatedStrReg.MatchString(frag) {
+		separated := strings.Split(frag, "|")
+		options := make([]option, len(separated))
+
+		for i, s := range separated {
+			options[i].str = s
+		}
+
+		return options
+	}
+
+	var name string
+
+	frag = nameStrReg.ReplaceAllStringFunc(frag, func(n string) string {
+		name = n[1:]
+		return ""
+	})
+
+	if len(frag) == 0 {
+		return []option{option{name: name}}
+	}
+
+	if surroundStrReg.MatchString(frag) {
+		frag = frag[1 : len(frag)-1]
+
+		if separatedStrReg.MatchString(frag) {
+			separated := strings.Split(frag, "|")
+			options := make([]option, len(separated))
+
+			for i, s := range separated {
+				options[i].name = name
+				options[i].str = s
+			}
+
+			return options
+		}
+
+		return []option{option{name: name, reg: regexp.MustCompile(frag).String()}}
+	}
+
+	panic(fmt.Sprintf("routing: Invalid frag: %v", frag))
 }
 
 func checkURL(url string) string {
--- routing_test.go
+++ routing_test.go
@@ -6,6 +6,75 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestParse(t *testing.T) {
+	assert := assert.New(t)
+
+	t.Run("should parse empty string", func(t *testing.T) {
+		options := parse("")
+		assert.Equal([]option{option{str: ""}}, options)
+	})
+
+	t.Run("should parse a simple string", func(t *testing.T) {
+		options := parse("test")
+
+		assert.Equal([]option{option{str: "test"}}, options)
+	})
+
+	t.Run("should parse a string including _ and -", func(t *testing.T) {
+		options := parse("t-es_t")
+
+		assert.Equal([]option{option{str: "t-es_t"}}, options)
+	})
+
+	t.Run("should parse a reg", func(t *testing.T) {
+		regStr := `^[\w\.-]+$`
+		options := parse("(" + regStr + ")")
+
+		assert.Equal(1, len(options))
+		assert.Equal("", options[0].name)
+		assert.Equal(regStr, options[0].reg)
+	})
+
+	t.Run("should parse a named param", func(t *testing.T) {
+		options := parse(":_id")
+
+		assert.Equal([]option{option{name: "_id"}}, options)
+	})
+
+	t.Run("should parse a named param with strings", func(t *testing.T) {
+		options := parse(":_id(test1|test2)")
+
+		assert.Equal([]option{
+			option{name: "_id", str: "test1"},
+			option{name: "_id", str: "test2"},
+		}, options)
+	})
+
+	t.Run("should parse a named param with regex", func(t *testing.T) {
+		regStr := `^[\w\.-]+$`
+		options := parse(":_id(" + regStr + ")")
+
+		assert.Equal(1, len(options))
+		assert.Equal("_id", options[0].name)
+		assert.Equal(regStr, options[0].reg)
+	})
+
+	t.Run("should parse a named param with strings", func(t *testing.T) {
+		options := parse("test1|test2")
+
+		assert.Equal([]option{
+			option{str: "test1"},
+			option{str: "test2"},
+		}, options)
+	})
+
+	t.Run("should panic on invalid strings", func(t *testing.T) {
+		assert.Panics(func() {
+			parse("test1|$$$")
+		})
+	})
+}
+
 func TestDefine(t *testing.T) {
 	assert := assert.New(t)
 
@@ -15,7 +84,7 @@ func TestDefine(t *testing.T) {
 		assert.Nil(root.parent)
 		assert.NotNil(root.children)
 
-		root.Define("/")
+		root.Define("/", nil)
 
 		assert.Equal(1, len(root.children))
 
@@ -26,15 +95,15 @@ func TestDefine(t *testing.T) {
 		root := New()
 
 		assert.Panics(func() {
-			root.Define("test")
+			root.Define("test", nil)
 		})
 	})
 
 	t.Run("should create the first level children", func(t *testing.T) {
 		root := New()
 
-		root.Define("/test1")
-		root.Define("/test2")
+		root.Define("/test1", nil)
+		root.Define("/test2", nil)
 
 		assert.Equal(2, len(root.children))
 
@@ -45,9 +114,9 @@ func TestDefine(t *testing.T) {
 	t.Run("should create the two level children", func(t *testing.T) {
 		root := New()
 
-		root.Define("/test1/test2")
-		root.Define("/test1/test3")
-		root.Define("/test1/:_id(test3|test4)")
+		root.Define("/test1/test2", nil)
+		root.Define("/test1/test3", nil)
+		root.Define("/test1/:_id(test3|test4)", nil)
 
 		assert.Equal(1, len(root.children))
 
@@ -67,8 +136,8 @@ func TestDefine(t *testing.T) {
 		root := New()
 		regStr := `^[\w\.-]+$`
 
-		root.Define("/(" + regStr + ")")
-		root.Define("/:_id(" + regStr + ")")
+		root.Define("/("+regStr+")", nil)
+		root.Define("/:_id("+regStr+")", nil)
 
 		assert.Equal(2, len(root.children))
 
@@ -79,8 +148,8 @@ func TestDefine(t *testing.T) {
 	t.Run("should create a separated strings url", func(t *testing.T) {
 		root := New()
 
-		root.Define("/test1|test2")
-		root.Define("/:_id(test1|test2)")
+		root.Define("/test1|test2", nil)
+		root.Define("/:_id(test1|test2)", nil)
 
 		assert.Equal(4, len(root.children))
 
@@ -88,5 +157,79 @@ func TestDefine(t *testing.T) {
 		assert.Equal(root, root.children[option{str: "test2"}].parent)
 		assert.Equal(root, root.children[option{str: "test1", name: "_id"}].parent)
 		assert.Equal(root, root.children[option{str: "test2", name: "_id"}].parent)
+	})
+}
+
+func TestMatch(t *testing.T) {
+	assert := assert.New(t)
+
+	t.Run("should match /", func(t *testing.T) {
+		root := New()
+
+		root.Define("/", 1)
+		n, p, ok := root.Match("/")
+
+		assert.True(ok)
+		assert.Zero(len(p))
+		assert.Equal(1, n.Callback.(int))
+	})
+
+	t.Run("should match one level url", func(t *testing.T) {
+		root := New()
+
+		root.Define("/fav.icon", 1)
+		n, p, ok := root.Match("/fav.icon")
+
+		assert.True(ok)
+		assert.Zero(len(p))
+		assert.Equal(1, n.Callback.(int))
+	})
+
+	t.Run("should match named params", func(t *testing.T) {
+		root := New()
+
+		root.Define("/:_id", 1)
+		n, p, ok := root.Match("/123")
+
+		assert.True(ok)
+		assert.Equal(1, len(p))
+		assert.Equal("123", p["_id"])
+		assert.Equal(1, n.Callback.(int))
+	})
+
+	t.Run("should match named params for regex", func(t *testing.T) {
+		root := New()
+
+		root.Define("/:_id(\\w{3,30})", 1)
+		n, p, ok := root.Match("/haha")
+
+		assert.True(ok)
+		assert.Equal(1, len(p))
+		assert.Equal("haha", p["_id"])
+		assert.Equal(1, n.Callback.(int))
+	})
+
+	t.Run("should match string first", func(t *testing.T) {
+		root := New()
+
+		root.Define("/haha", 1)
+		root.Define("/(\\w{3,30})", 2)
+		n, p, ok := root.Match("/haha")
+
+		assert.True(ok)
+		assert.Equal(0, len(p))
+		assert.Equal(1, n.Callback.(int))
+	})
+
+	t.Run("should match encoded url", func(t *testing.T) {
+		root := New()
+
+		root.Define("/:_id(@\\w+)", 1)
+		n, p, ok := root.Match("/%40haha")
+
+		assert.True(ok)
+		assert.Equal(1, len(p))
+		assert.Equal("@haha", p["_id"])
+		assert.Equal(1, n.Callback.(int))
 	})
 }
